16dfc6d9b16703cfb7c7c7c2345cfb16daebefa2
Sang Yum <syum@amplifyfederal.com>
2025-07-31 09:37:14 -0700

    added descriptoin

--- src/stories/project/userStoryForm.stories.ts
+++ src/stories/project/userStoryForm.stories.ts
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react-vite';
 import { fn } from 'storybook/test';
 import UserStoryForm from '../../pages/project/userStoryForm';
-import type { UserStory, StoryType } from '../../models/userStory';
+import type { UserStory } from '../../models/userStory';
 import type { User } from '../../models/user';
 
 // Mock user data
